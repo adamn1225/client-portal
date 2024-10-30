69615b8519063b128b84100c20bdbc5f4a203000
adamn1225 <anoah1225@gmail.com>
2024-10-30 17:28:07 -0400

    idk

--- next.config.mjs
+++ next.config.mjs
@@ -4,16 +4,6 @@ const nextConfig = {
   images: {
     domains: ['fazytsvctdzbhvsavvwj.supabase.co', 'www.gravatar.com'],
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // Fixes npm packages that depend on `fs` module
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
