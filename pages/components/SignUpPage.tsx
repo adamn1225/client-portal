29926537bba61955ee7630ad1cae75ced01ad3a9
adamn1225 <anoah1225@gmail.com>
2024-11-05 13:26:06 -0500

    feedback button

--- pages/components/SignUpPage.tsx
+++ pages/components/SignUpPage.tsx
@@ -140,137 +140,137 @@ export default function SignUpPage() {
                             </Link>
                         </div>
                         <div className=" w-full text-gray-900 h-full sm:h-auto sm:w-full max-w-md p-5 bg-white shadow flex flex-col justify-center items-center text-base">
-                    <h2 className="mt-12 md:mt-0 text-2xl font-bold text-center">SSTA Inc</h2>
-                    <div className="xs:w-2/5 md:w-full h-full sm:h-auto p-5 bg-white shadow flex flex-col text-base">
-                        <span className="font-sans text-4xl text-center pb-2 mb-1 border-b mx-4 align-center">
-                            Sign Up
-                        </span>
-                        {error && <div className="text-red-500 text-center mb-4">{error}</div>}
-                        {success ? (
-                            <div className="text-green-500 text-center mb-4 border border-slate-900 p-4 rounded">
-                                Your sign up was successful! Please check your email to confirm your account. Make sure to check your spam or junk folder if you don&apos;t see it within two minutes!
-                            </div>
-                        ) : (
-                            <form className="mt-4" onSubmit={handleSignUp}>
-                                <label htmlFor="firstName">First Name</label>
-                                <input
-                                    type="text"
-                                    id="firstName"
-                                    value={firstName}
-                                    onChange={(e) => setFirstName(e.target.value)}
-                                    required
-                                    className="w-full p-2 mt-2 border rounded"
-                                    disabled={loading}
-                                />
-                                <label htmlFor="lastName" className="mt-4">Last Name</label>
-                                <input
-                                    type="text"
-                                    id="lastName"
-                                    value={lastName}
-                                    onChange={(e) => setLastName(e.target.value)}
-                                    required
-                                    className="w-full p-2 mt-2 border rounded"
-                                    disabled={loading}
-                                />
-                                <label htmlFor="companyName" className="mt-4">Company Name</label>
-                                <input
-                                    type="text"
-                                    id="companyName"
-                                    value={companyName}
-                                    onChange={(e) => setCompanyName(e.target.value)}
-                                    className="w-full p-2 mt-2 border rounded"
-                                    disabled={loading}
-                                />
-                                <label htmlFor="companySize" className="mt-4">Company Size</label>
-                                <select
-                                    id="companySize"
-                                    value={companySize}
-                                    onChange={(e) => setCompanySize(e.target.value)}
-                                    className="w-full p-2 mt-2 border rounded"
-                                    disabled={loading}
-                                >
-                                    <option value="">Select Company Size</option>
-                                    <option value="1-10">1-10</option>
-                                    <option value="11-50">11-50</option>
-                                    <option value="51-200">51-200</option>
-                                    <option value="201-500">201-500</option>
-                                    <option value="501-1000">501-1000</option>
-                                    <option value="1001+">1001+</option>
-                                </select>
-                                <label htmlFor="email" className="mt-4">Email</label>
-                                <input
-                                    type="email"
-                                    id="email"
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                    required
-                                    className="w-full p-2 mt-2 border rounded"
-                                    disabled={loading}
-                                />
-                                <label htmlFor="password" className="mt-4">Password</label>
-                                <input
-                                    type="password"
-                                    id="password"
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    required
-                                    className="w-full p-2 mt-2 border rounded"
-                                    disabled={loading}
-                                />
-                                <label htmlFor="confirmPassword" className="mt-4">Confirm Password</label>
-                                <input
-                                    type="password"
-                                    id="confirmPassword"
-                                    value={confirmPassword}
-                                    onChange={(e) => setConfirmPassword(e.target.value)}
-                                    required
-                                    className="w-full p-2 mt-2 border rounded"
-                                    disabled={loading}
-                                />
-                                <div className='flex gap-1 items-center my-2'>
-                                    <label htmlFor="inviteOthers" className="mt-1">Invite Others</label>
-                                    <input
-                                        type="checkbox"
-                                        id="inviteOthers"
-                                        checked={inviteOthers}
-                                        onChange={(e) => setInviteOthers(e.target.checked)}
-                                        className="mt-2"
-                                        disabled={loading}
-                                    />
-                                </div>
-                                {inviteOthers && (
-                                    <div className="mt-4">
-                                        <label htmlFor="inviteEmails">Invite Emails (comma separated)</label>
-                                        <textarea
-                                            id="inviteEmails"
-                                            value={inviteEmails}
-                                            onChange={(e) => setInviteEmails(e.target.value)}
+                            <h2 className="mt-12 md:mt-0 text-2xl font-bold text-center">SSTA Inc</h2>
+                            <div className="xs:w-2/5 md:w-full h-full sm:h-auto p-5 bg-white shadow flex flex-col text-base">
+                                <span className="font-sans text-4xl text-center pb-2 mb-1 border-b mx-4 align-center">
+                                    Sign Up
+                                </span>
+                                {error && <div className="text-red-500 text-center mb-4">{error}</div>}
+                                {success ? (
+                                    <div className="text-green-500 text-center mb-4 border border-slate-900 p-4 rounded">
+                                        Your sign up was successful! Please check your email to confirm your account. Make sure to check your spam or junk folder if you don&apos;t see it within two minutes!
+                                    </div>
+                                ) : (
+                                    <form className="mt-4" onSubmit={handleSignUp}>
+                                        <label htmlFor="firstName">First Name</label>
+                                        <input
+                                            type="text"
+                                            id="firstName"
+                                            value={firstName}
+                                            onChange={(e) => setFirstName(e.target.value)}
+                                            required
                                             className="w-full p-2 mt-2 border rounded"
                                             disabled={loading}
                                         />
-                                    </div>
-                                )}
-                                <button
-                                    type="submit"
-                                    className="w-full light-dark-btn"
-                                    disabled={loading}
-                                >
-                                    {loading ? 'Signing Up...' : 'Sign Up'}
-                                </button>
+                                        <label htmlFor="lastName" className="mt-4">Last Name</label>
+                                        <input
+                                            type="text"
+                                            id="lastName"
+                                            value={lastName}
+                                            onChange={(e) => setLastName(e.target.value)}
+                                            required
+                                            className="w-full p-2 mt-2 border rounded"
+                                            disabled={loading}
+                                        />
+                                        <label htmlFor="companyName" className="mt-4">Company Name</label>
+                                        <input
+                                            type="text"
+                                            id="companyName"
+                                            value={companyName}
+                                            onChange={(e) => setCompanyName(e.target.value)}
+                                            className="w-full p-2 mt-2 border rounded"
+                                            disabled={loading}
+                                        />
+                                        <label htmlFor="companySize" className="mt-4">Company Size</label>
+                                        <select
+                                            id="companySize"
+                                            value={companySize}
+                                            onChange={(e) => setCompanySize(e.target.value)}
+                                            className="w-full p-2 mt-2 border rounded"
+                                            disabled={loading}
+                                        >
+                                            <option value="">Select Company Size</option>
+                                            <option value="1-10">1-10</option>
+                                            <option value="11-50">11-50</option>
+                                            <option value="51-200">51-200</option>
+                                            <option value="201-500">201-500</option>
+                                            <option value="501-1000">501-1000</option>
+                                            <option value="1001+">1001+</option>
+                                        </select>
+                                        <label htmlFor="email" className="mt-4">Email</label>
+                                        <input
+                                            type="email"
+                                            id="email"
+                                            value={email}
+                                            onChange={(e) => setEmail(e.target.value)}
+                                            required
+                                            className="w-full p-2 mt-2 border rounded"
+                                            disabled={loading}
+                                        />
+                                        <label htmlFor="password" className="mt-4">Password</label>
+                                        <input
+                                            type="password"
+                                            id="password"
+                                            value={password}
+                                            onChange={(e) => setPassword(e.target.value)}
+                                            required
+                                            className="w-full p-2 mt-2 border rounded"
+                                            disabled={loading}
+                                        />
+                                        <label htmlFor="confirmPassword" className="mt-4">Confirm Password</label>
+                                        <input
+                                            type="password"
+                                            id="confirmPassword"
+                                            value={confirmPassword}
+                                            onChange={(e) => setConfirmPassword(e.target.value)}
+                                            required
+                                            className="w-full p-2 mt-2 border rounded"
+                                            disabled={loading}
+                                        />
+                                        <div className='flex gap-1 items-center my-2'>
+                                            <label htmlFor="inviteOthers" className="mt-1">Invite Others</label>
+                                            <input
+                                                type="checkbox"
+                                                id="inviteOthers"
+                                                checked={inviteOthers}
+                                                onChange={(e) => setInviteOthers(e.target.checked)}
+                                                className="mt-2"
+                                                disabled={loading}
+                                            />
+                                        </div>
+                                        {inviteOthers && (
+                                            <div className="mt-4">
+                                                <label htmlFor="inviteEmails">Invite Emails (comma separated)</label>
+                                                <textarea
+                                                    id="inviteEmails"
+                                                    value={inviteEmails}
+                                                    onChange={(e) => setInviteEmails(e.target.value)}
+                                                    className="w-full p-2 mt-2 border rounded"
+                                                    disabled={loading}
+                                                />
+                                            </div>
+                                        )}
+                                        <button
+                                            type="submit"
+                                            className="w-full light-dark-btn"
+                                            disabled={loading}
+                                        >
+                                            {loading ? 'Signing Up...' : 'Sign Up'}
+                                        </button>
 
-                        <div className='flex flex-col justify-evenly max-h-max items-center w-full my-4'>
-                            <div className='border-t border-gray-900/40 pt-1 mb-2 w-full text-center'><h3>Already have an account?</h3></div>
-                            <Link href="/login" legacyBehavior>
-                                <a className="text-center text-lg font-semibold text-gray-700 hover:underline underline-offset-2 px-4 py-2 hover:text-slate-900/70">Login</a>
-                            </Link>
+                                        <div className='flex flex-col justify-evenly max-h-max items-center w-full my-4'>
+                                            <div className='border-t border-gray-900/40 pt-1 mb-2 w-full text-center'><h3>Already have an account?</h3></div>
+                                            <Link href="/login" legacyBehavior>
+                                                <a className="text-center text-lg font-semibold text-gray-700 hover:underline underline-offset-2 px-4 py-2 hover:text-slate-900/70">Login</a>
+                                            </Link>
+                                        </div>
+                                    </form>
+                                )}
+                            </div>
                         </div>
-                            </form>
-                        )}
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
         </>
     );
 }
