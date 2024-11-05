9c6c9f64820b5e88f8bcf5c0e30c1a9d6791469d
adamn1225 <anoah1225@gmail.com>
2024-11-05 15:19:08 -0500

    sign up errors

--- pages/components/UserSideNav.tsx
+++ pages/components/UserSideNav.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useRouter } from "next/router";
+import { useRouter } from 'next/router';
 import { useSupabaseClient } from '@supabase/auth-helpers-react';
 import { Database } from '@/lib/schema';
 import { useUser } from '@/context/UserContext';
@@ -16,6 +16,7 @@ interface UserSideNavProps {
 const UserSideNav: React.FC<UserSideNavProps> = ({ isSidebarOpen, toggleSidebar, className = '' }) => {
     const supabase = useSupabaseClient<Database>();
     const { userProfile } = useUser();
+    const router = useRouter();
 
     const handleLogout = async () => {
         try {
@@ -23,19 +24,19 @@ const UserSideNav: React.FC<UserSideNavProps> = ({ isSidebarOpen, toggleSidebar,
             if (error) {
                 console.error('Error logging out:', error.message);
                 alert('Failed to log out. Please try again.');
+            } else {
+                router.push('/login'); // Redirect to login page
             }
-            window.location.href = '/login'; // Redirect to login page
         } catch (err) {
             console.error('Unexpected error during logout:', err);
             alert('An unexpected error occurred. Please try again.');
-            window.location.href = '/login'; // Redirect to login page
+            router.push('/login'); // Redirect to login page
         }
     };
 
     const profilePictureUrl = userProfile?.profile_picture
         ? supabase.storage.from('profile-pictures').getPublicUrl(userProfile.profile_picture).data.publicUrl
         : 'https://www.gravatar.com/avatar?d=mp&s=100';
-    const router = useRouter();
 
     return (
         <>
@@ -48,7 +49,7 @@ const UserSideNav: React.FC<UserSideNavProps> = ({ isSidebarOpen, toggleSidebar,
                         {isSidebarOpen ? <PanelRightClose size={24} className='text-white z-50 drop-shadow-lg' /> : <PanelLeftOpen size={28} className='z-50 text-gray-900 dark:text-slate-100 drop-shadow-lg ' />}
                     </button>
                 </div>
-                <nav className={`side-navbar  z-50  flex flex-col h-screen py-6 drop-shadow absolute top-0 left-0 transform ${isSidebarOpen ? 'translate-x-0 z-50' : '-translate-x-full'} transition-transform duration-300 h-screen  ease-in-out z-50 ${className}`}>
+                <nav className={`side-navbar z-50 flex flex-col h-screen py-6 drop-shadow absolute top-0 left-0 transform ${isSidebarOpen ? 'translate-x-0 z-50' : '-translate-x-full'} transition-transform duration-300 h-screen ease-in-out z-50 ${className}`}>
                     <h1 className='text-xl mt-4 md:mt-0 mb-4 self-center dark:text-amber-400 font-semibold underline underline-offset-2 tracking-wider'>SSTA INC</h1>
                     <div className="w-full flex flex-col items-center gap-1 justify-center mb-6 border-b border-stone-100/40 pb-4">
                         <h3>Welcome {userProfile?.first_name || 'User'}</h3>
@@ -56,35 +57,35 @@ const UserSideNav: React.FC<UserSideNavProps> = ({ isSidebarOpen, toggleSidebar,
                     <ul className='flex gap-3 flex-col flex-grow space-y-1 overflow-y-auto'>
                         <li className={`w-full flex justify-center mt-0 ${router.pathname == "/user/inventory" ? "active" : ""}`}>
                             <Link href="/user/inventory" className={`side-nav-btn text-stone-100 font-semibold py-1 w-full ${router.pathname == "/user/inventory" ? "active" : ""}`}>
-                                <span className='flex items-center flex-nowrap justify-normal gap-2 text-xs'><ListCollapse size={'20px'}/> <span className='text-xs md:text-sm'>Inventory Management</span></span>
+                                <span className='flex items-center flex-nowrap justify-normal gap-2 text-xs'><ListCollapse size={'20px'} /> <span className='text-xs md:text-sm'>Inventory Management</span></span>
                             </Link>
                         </li>
                         <li className={`w-full flex justify-normal m-0 ${router.pathname == "/user/freight-rfq" ? "active" : ""}`}>
                             <Link href="/user/freight-rfq" className={`side-nav-btn text-stone-100 font-semibold py-1 w-full ${router.pathname == "/user/freight-rfq" ? "active" : ""}`}>
-                                <span className='flex items-center flex-nowrap justify-normal gap-2'><Workflow size={'20px'}/> <span className='text-xs md:text-sm'>Logistics RFQ </span></span>
+                                <span className='flex items-center flex-nowrap justify-normal gap-2'><Workflow size={'20px'} /> <span className='text-xs md:text-sm'>Logistics RFQ </span></span>
                             </Link>
                         </li>
                         <li className={`w-full flex justify-normal m-0 ${router.pathname == "/user/user-documents" ? "active" : ""}`}>
                             <Link href="/user/user-documents" className={`side-nav-btn text-stone-100 font-semibold py-1 w-full ${router.pathname == "/user/user-documents" ? "active" : ""}`}>
-                                <span className='flex items-center flex-nowrap justify-normal gap-2'><Folders size={'20px'}/> <span className='text-xs md:text-sm'>Documents </span></span>
+                                <span className='flex items-center flex-nowrap justify-normal gap-2'><Folders size={'20px'} /> <span className='text-xs md:text-sm'>Documents </span></span>
                             </Link>
                         </li>
 
                         <li className={`w-full flex justify-normal m-0 ${router.pathname == "/user/procurement" ? "active" : ""}`}>
                             <Link href="/user/procurement" className={`side-nav-btn text-stone-100 font-semibold py-1 w-full ${router.pathname == "/user/procurement" ? "active" : ""}`}>
-                            <span className='w-full flex items-center flex-nowrap justify-normal gap-2'><Signature size={'20px'}/> <span className='text-xs md:text-sm'>Procurement </span></span>
+                                <span className='w-full flex items-center flex-nowrap justify-normal gap-2'><Signature size={'20px'} /> <span className='text-xs md:text-sm'>Procurement </span></span>
                             </Link>
                         </li>
 
                         <li className="w-full flex justify-normal m-0">
                             <Link href="/" className="side-nav-btn text-stone-100 text-nowrap font-semibold py-1 w-full">
-                                <span className='w-full flex items-center flex-nowrap justify-normal gap-2'><Hammer size={'20px'}/> <span className='text-xs md:text-sm'>Projects </span></span>
+                                <span className='w-full flex items-center flex-nowrap justify-normal gap-2'><Hammer size={'20px'} /> <span className='text-xs md:text-sm'>Projects </span></span>
                             </Link>
                         </li>
 
                         <li className="w-full flex justify-normal m-0">
-                            <Link href="/" className="side-nav-btn  text-stone-100 text-nowrap font-semibold py-1 w-full">
-                                <span className='w-full flex items-center flex-nowrap justify-normal gap-2'><Users size={'20px'}/> <span className='text-xs md:text-sm'>Members</span></span>
+                            <Link href="/" className="side-nav-btn text-stone-100 text-nowrap font-semibold py-1 w-full">
+                                <span className='w-full flex items-center flex-nowrap justify-normal gap-2'><Users size={'20px'} /> <span className='text-xs md:text-sm'>Members</span></span>
                             </Link>
                         </li>
                     </ul>
@@ -94,8 +95,7 @@ const UserSideNav: React.FC<UserSideNavProps> = ({ isSidebarOpen, toggleSidebar,
                                 <Settings />   Settings
                             </Link>
                         </li>
-                        <li className="w-full flex items-centerjustify-center m-0">
-
+                        <li className="w-full flex items-center justify-center m-0">
                             <button className="logout dark:bg-gray-300 dark:text-gray-700 font-semibold py-1 w-full" onClick={handleLogout}>
                                 Logout
                             </button>
