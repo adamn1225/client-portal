50d26862dbccaaca99678b0e5063f24ab71f89a7
adamn1225 <anoah1225@gmail.com>
2024-11-11 09:16:52 -0500

    home topnav responsive

--- pages/components/TopNavbar.tsx
+++ pages/components/TopNavbar.tsx
@@ -12,8 +12,8 @@ interface TopNavbarProps {
 const TopNavbar: React.FC<TopNavbarProps> = ({ className }) => {
     return (
         <header className="bg-gray-900 dark:bg-gray-950 text-white p-4">
-            <div className="container w-full mx-auto flex flex-col md:flex-row justify-between items-center gap-4">
-                <span className="sm:text-base sm:flex sm:flex-col md:flex-row md:justify-start  items-center text-slate-900 font-bold w-full md:text-3xl md:gap-6 dark:text-slate-100">
+            <div className="md:container w-full mx-auto flex flex-col xs:justify-center md:flex-row md:justify-between items-center gap-4">
+                <span className="sm:text-base flex flex-row xs:justify-between md:justify-start  items-center text-slate-900 font-bold w-full md:text-3xl md:gap-6 dark:text-slate-100">
                     <span className='flex items-center justify-center font-bold  flex-nowrap'><Move3d className='size-10 text-red-800' /> 
                      <Link href="/"><h1 className='tracking-tighter text-stone-100 text-nowrap font-serif md:leading-10'>HEAVY CONSTRUCT</h1></Link>
                      </span>
@@ -24,7 +24,7 @@ const TopNavbar: React.FC<TopNavbarProps> = ({ className }) => {
                     <Link href="/login" className="md:ml-4 dark-light-btn">Sign In</Link>
                     <Link href="/signup" className="ml-4 dark-light-btn">Sign Up</Link>
                 </nav>
-                <Link href="/contact" className="ml-12 m-0 contact-btn">Contact Us</Link>
+                <Link href="/contact" className="object-center md:ml-12 m-0 contact-btn">Contact Us</Link>
             </div>
         </header>
     );
